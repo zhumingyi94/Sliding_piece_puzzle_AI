6642807a28dbad9f719b9ba5d4ec42a396ff8bf8
nhatdm22022537 <dominhnhat2004@gmail.com>
2023-09-20 05:09:26 +0700

    fix to deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,38 @@
-import React from "react"
-import styles from './nav.module.css'
+import React from "react";
+import styles from './nav.module.css';
 
+interface ButtonProps {
+  text: string;
+  to: string;
+}
 
-
-function Button({text, to}) {
-    return (
-        <a href={to}>
-        <button className = {styles.nav_button}>
-            {text}
-        </button>
-        </a>
-    );
+function Button({ text, to }: ButtonProps) {
+  return (
+    <a href={to}>
+      <button className={styles.nav_button}>
+        {text}
+      </button>
+    </a>
+  );
 }
 
 function Navigation() {
-    return (
-      <div>
-        <h1 className = {styles.sth}>CHOOSE SIZE</h1>
-        <div className = {styles.container}>
-        <Button text="3x3" to = "./3x3"/>
-        <Button text="4x4" to = "./4x4"/>
-        <Button text="5x5" to = "./5x5"/>
-        </div>
+  return (
+    <div>
+      <h1 className={styles.sth}>CHOOSE SIZE</h1>
+      <div className={styles.container}>
+        <Button text="3x3" to="./3x3" />
+        <Button text="4x4" to="./4x4" />
+        <Button text="5x5" to="./5x5" />
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default function FullPage() {
-    return(
+  return (
     <div className={styles.pageContainer}>
-        <Navigation />
-      </div>
-    );
+      <Navigation />
+    </div>
+  );
 }
